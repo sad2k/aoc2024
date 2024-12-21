38d1cb451338cada3c472e343c6eaadc74b30427
Sergey Dubakov <serge.dubakov@gmail.com>
2024-12-21 14:37:40 +0000

    Day 2 part 1

---
+++ src/bin/day2.rs
@@ -0,0 +1,33 @@
+use std::fs;
+use std::str::Lines;
+
+fn parse(lines: Lines<'_>) -> Vec<Vec<i32>> {
+    lines.map(|line| line.split(" ").map(|x| x.parse::<i32>().unwrap())
+        .collect::<Vec<_>>()).collect::<Vec<_>>()
+}
+
+fn is_safe(report: &Vec<i32>) -> bool {
+    let pairs = report.windows(2).collect::<Vec<_>>();
+    let deltas = pairs.iter().map(|x| (x[1] - x[0]).abs()).collect::<Vec<_>>();
+    let bad_deltas = deltas.iter().filter(|x| **x < 1 || **x > 3);
+    if bad_deltas.count() > 0 {
+        return false;
+    }
+    let signs = pairs.iter().map(|x| (x[1] - x[0]).signum()).collect::<Vec<_>>();
+    if signs.iter().sum::<i32>().abs() != signs.len() as i32 {
+        return false;
+    }
+    true
+}
+
+fn part1(input: &Vec<Vec<i32>>) -> u32 {
+    input.iter().filter(|x| is_safe(*x)).count() as u32
+}
+
+fn main() {
+    let contents = fs::read_to_string("inputs/day2.txt").unwrap();
+    let parsed = parse(contents.lines());
+
+    // part 1
+    println!("{}", part1(&parsed));
+}
